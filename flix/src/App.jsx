fdf889cca4f5bf41440c4a61381149e59dd5076e
Holardeeps <praiseelijah2001@gmail.com>
2025-08-02 21:16:25 +0100

    Adding the appwrite database service

--- flix/src/App.jsx
+++ flix/src/App.jsx
@@ -1,7 +1,9 @@
-import { use, useEffect, useState } from "react"
-import Search from "./components/Search"
+import { useEffect, useState } from 'react';
+import { useDebounce } from 'react-use'
+import Search from "./components/Search";
 import Spinner from "./components/Spinner";
 import MovieCard from "./components/MovieCard";
+import { updateSearchCount } from './appwrite';
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
@@ -18,15 +20,20 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [movieList, setMovieList] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('');
 
-  const fetchMovies = async () => {
+  // Debounce prevents sending API calls for every letter the user types and waits for the user to stop typing after a specified time: in this case 500ms
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm]);
+
+  const fetchMovies = async (query = '') => {
 
     setIsLoading(true);
     setErrorMessage('');
 
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      // We declare the endpoint with a conditional literal so that when theres an available query passed by the user to be searched for we can run a URL and also when we are only getting from the API. the query is passed into the encodeURI() to convert the query from a string to a URI code
+      const endpoint = query ? `${API_BASE_URL}/search/movie?query=$${encodeURIComponent(query)}` : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
       const response = await fetch(endpoint, API_OPTIONS);
 
       if(!response.ok) {
@@ -44,6 +51,10 @@ function App() {
 
       setMovieList(data.results || []);
 
+      if(query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0])
+      }
+
     } catch (error) {
       console.error(`Error fetching movies: ${error.message}`);
       setErrorMessage("Failed to fetch movies. Please try again later.");
@@ -53,8 +64,8 @@ function App() {
   }
 
   useEffect(() => {
-    fetchMovies();
-  }, [])
+    fetchMovies(debouncedSearchTerm);
+  }, [debouncedSearchTerm])
 
   return (
     <main>
@@ -67,7 +78,7 @@ function App() {
             <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
 
             <Search 
-              searchTerm={searchTerm}
+              setDebouncedSearchTerm={searchTerm}
               setSearchTerm={setSearchTerm}
             />
           </header>
